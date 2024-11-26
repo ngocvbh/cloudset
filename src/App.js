a9850d34eeece8361d92d1dd19b5cc3835e3e25d
SPerezdeTudela <sperezde@uci.edu>
2024-11-25 22:54:54 -0800

    File name changes, WeatherData calls the weather API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import HomePage from './components/home_page/Home_Page.js';
+import SearchScreen from './components/search_screen/SearchScreen.js'
 
 function App() {
   return (
