092930c838a9438c130bd266fa41357146b4d678
SPerezdeTudela <sperezde@uci.edu>
2024-12-04 15:35:23 -0800

    Added moving to home page by pressing the enter button

--- src/components/search_screen/SearchBar.js
+++ src/components/search_screen/SearchBar.js
@@ -2,8 +2,12 @@
 
 import './SearchBar.css';
 import SearchIcon from './Search.png';
+import {useNavigate} from 'react-router-dom';
 
 function SearchBar(props){
+
+    // useNavigate lets you navigate w/out the user interaction
+    const navigate = useNavigate();
 
     // Changes the location based on what the user types
     const changeLocation = (evnt) =>{
@@ -11,11 +15,20 @@ function SearchBar(props){
         props.setCity(newCity);
     }
 
+    // When the user hits enter, they go to the next page as well
+    const goHome = (evnt) =>{
+        console.log("Trying to go home");
+        // 13 means the enter key
+        if (evnt.keyCode === 13){
+            navigate('/home_page');
+        }
+    }
+
     return (
         <div id="search-bar">
             <img src={SearchIcon} alt="search icon"/>
             <input type="text" id="search-query" placeholder="Enter a zip code or city"
-                    onChange={changeLocation}/>
+                   onChange={changeLocation} onKeyDown={goHome}/>
         </div>   
     );
 }
--- src/components/search_screen/SearchButton.js
+++ src/components/search_screen/SearchButton.js
@@ -7,9 +7,9 @@ import {Outlet, Link} from "react-router-dom";
 function SearchButton() {
     return(
         <div id="search">
-                <Link id="search-button" to="/home_page">Search</Link>
-                <Outlet />
-            </div>
+            <Link id="search-button" to="/home_page">Search</Link>
+            <Outlet />
+        </div>
     );
 }
 
