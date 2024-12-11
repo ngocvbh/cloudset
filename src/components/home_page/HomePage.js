c9290e1aeceb52ac3f29bec1fc2beccf8fba2205
Ngoc Huynh <97254009+ngocvbh@users.noreply.github.com>
2024-12-11 15:07:03 -0800

    Added back to search button

--- src/components/home_page/HomePage.js
+++ src/components/home_page/HomePage.js
@@ -3,9 +3,11 @@ import './HomePage.css';
 import Title from '../Title.js';
 import Recommendation from './Recommendation.js';
 import WeatherSection from './WeatherSection.js';
+import { useNavigate } from 'react-router-dom';
 
 function HomePage(props) {
     const [temp, setTemp] = useState(null);
+    const navigate = useNavigate();
 
     return (
         <div id="home-page">
@@ -16,6 +18,13 @@ function HomePage(props) {
                 onWeatherData={(temperature) => setTemp(temperature)} 
             />
             {temp !== null && <Recommendation temp={temp} />}
+
+            <button 
+                id="back-to-search-button" 
+                onClick={() => navigate('/')} // Navigate to search page
+            >
+                Back to Search
+            </button>
         </div>
     );
 }
